2b55aa1cfcb399a71a6a8c873b1874037a0fbf77
PresidentProtein <presidentprotein@protonmail.com>
2022-05-02 14:31:33 +0800

    Rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PUBLIC_MINT_START_DATE = "2022-05-05T11:30:48+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0x8C514BF6bC929463FF0a2617398Fb7ea6Eeb302F9"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x8C514BF6bC929463FF0a2617398Fb7ea6Eeb302F"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x84F606aaA1423a0842511D80Ee7B82c5e58320f1"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Matchstick Guy NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Matchstick Guy NFT Project is a decentralized, click <a href="https://linktr.ee/"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> to see more in our linktree.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
